ef50217af27effdff1f49e1f56c5200ae2b0b002
Johan Hanses <johanhanses@gmail.com>
2021-11-20 11:57:13 +0100

    ui error

--- resources/views/components/menu-icon.blade.php
+++ resources/views/components/menu-icon.blade.php
@@ -1,7 +1,7 @@
 <svg
     x-show="! show"
     viewBox="0 0 100 80"
-    class="text-white fill-current h-8 w-8 lg:hidden"
+    class="text-white fill-current h-8 w-8"
 >
     <rect width="100" height="12" rx="0"></rect>
     <rect y="30" width="100" height="12" rx="0"></rect>
